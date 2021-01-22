0cd6e3c6d6a5761f079fb7a8c0ebc5691a81cfb6
Peter Young <young40@gmail.com>
2021-01-23 02:49:31 +0800

    auto copy dll,pdb after compiled.

--- Assets/GameScript/GameInit.cs
+++ Assets/GameScript/GameInit.cs
@@ -9,6 +9,7 @@ public class GameInit
     public static void Start()
     {
         Debug.Log("Hello from ILRuntime.");
+        Debug.Log("Hello from ILRuntime.");
 
         var core = XCore.GetMainInstance();
         var uikit = core.Services.Get<IUIKit>();
---
+++ Assets/Scripts/Editor/CopyDllAfterCompiled.cs
@@ -0,0 +1,30 @@
+using TinaX;
+using TinaX.XILRuntime.Const;
+using TinaX.XILRuntime.Internal;
+using UnityEngine;
+
+public class CopyDllAfterCompiled
+{
+    [UnityEditor.Callbacks.DidReloadScripts]
+    static void CopyDLL()
+    {
+        var config = XConfig.GetConfig<XILRTConfig>(XILConst.ConfigPath_Resources);
+
+        if (config == null)
+        {
+            Debug.LogError("Config file not found :" + XILConst.ConfigPath_Resources);
+            return;
+        }
+
+        var dll = System.Environment.CurrentDirectory + "/" + config.LoadAssemblies[0].AssemblyPath;
+        var pdb = System.Environment.CurrentDirectory + "/" + config.LoadAssemblies[0].SymbolPath;
+
+        var srcDll = System.Environment.CurrentDirectory + "/Library/ScriptAssemblies/GameBrick.dll";
+        var srcPdb = System.Environment.CurrentDirectory + "/Library/ScriptAssemblies/GameBrick.pdb";
+        
+        System.IO.File.Copy(srcDll, dll, true);
+        System.IO.File.Copy(srcPdb, pdb, true);
+        
+        Debug.Log("GameScript DLL updated.");
+    }
+}
