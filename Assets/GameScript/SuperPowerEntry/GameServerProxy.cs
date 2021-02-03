eb5ca15e290cae35da2b2f6cf3c693194a40f6f9
Peter Young <young40@gmail.com>
2021-02-03 23:54:47 +0800

    add gamesevervo read

--- Assets/GameScript/SuperPowerEntry/GameServerProxy.cs
+++ Assets/GameScript/SuperPowerEntry/GameServerProxy.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using PureMVC.Patterns.Proxy;
 using UniRx;
 
@@ -6,15 +9,53 @@ namespace SuperPowerEntry
     public class GameServerProxy: Proxy
     {
         public static string NAME = nameof(GameServerProxy);
+
+        private readonly string filePath = Path.Combine(UnityEngine.Application.dataPath, "SpeDebug", "GameServers.dat");
         
         public GameServerProxy() : base(NAME, new ReactiveCollection<GameServerVO>())
         {
-            
+            var rs = this.TryGetData();
+
+            if (!rs)
+            {
+                ServerList.Add(new GameServerVO("测试服", "1.1.1.1", 8000));
+                ServerList.Add(new GameServerVO("线上服", "2.2.2.2", 8001));
+            }
         }
 
         public ReactiveCollection<GameServerVO> ServerList
         {
             get => (ReactiveCollection<GameServerVO>) Data;
+        }
+
+        private bool TryGetData()
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    var obj = new ReactiveCollection<GameServerVO>();
+                    var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+                    var bf = new BinaryFormatter();
+
+                    obj = bf.Deserialize(fs) as ReactiveCollection<GameServerVO>;
+                    
+                    fs.Close();
+
+                    Data = obj;
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            
+            return false;
+        }
+
+        private void SaveData()
+        {
+            
         }
     }
 }
